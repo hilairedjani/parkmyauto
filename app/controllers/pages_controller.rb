b18a08e14b2377bf95cc021be610c6d415f2d35d
hilairedjani <djanilewis1@gmail.com>
2019-05-13 10:03:55 -0400

    Display vehicles on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
     def home
+        @vehicles = Vehicle.where(user_id: current_user.id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     get "login", to: "devise/sessions#new"
     get "register", to: "devise/registrations#new"
   end
+
+  resources :vehicles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 end
