57ad1c87f0351303b159c0583e282e2a42e3d30a
hilairedjani <djanilewis1@gmail.com>
2019-05-13 02:39:20 -0400

    Configure navigation helper

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module ParkMyAuto
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Helper modules are specific to avoid name clash
+    config.action_controller.include_all_helpers = false
   end
 end
