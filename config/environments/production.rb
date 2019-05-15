c96da64355e73de967cfdcd478491367c626d655
hilairedjani <djanilewis1@gmail.com>
2019-05-15 14:41:59 -0400

    Modify domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => 'hilairedjani',
     :password => 'Hilaire6*',
-    :domain => 'parkmyauto.herokuapp.com/',
+    :domain => 'parkmyauto.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
