be5f8f664a7991934b43700f8a57488abeca8cc5
hilairedjani <djanilewis1@gmail.com>
2019-05-15 14:35:22 -0400

    Set up smtp

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => 'hilairedjani',
     :password => 'Hilaire6*',
-    :domain => 'localhost:3000',
+    :domain => 'parkmyauto.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
