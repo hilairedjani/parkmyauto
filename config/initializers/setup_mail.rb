234c2062e40a1f9053c7e186d54fc11cb6e785d2
hilairedjani <djanilewis1@gmail.com>
2019-05-15 14:48:26 -0400

    Set up sendgrid for production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,10 @@
-# if Rails.env.development? || Rails.env.production?
-#     ActionMailer::Base.delivery_method = :smtp
-#     ActionMailer::Base.smtp_settings = {
-#       address:        'smtp.sendgrid.net',
-#       port:           2525,
-#       authentication: :plain,
-#       user_name:      'hilairedjani',
-#       password:       'Hilaire6*',
-#       domain:          "sendgrid.com",
-#       enable_starttls_auto: true
-#     }
-# end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'app133672688@heroku.com',
+  :password             =>  '7erqerni3546',
+  :domain               =>  'heroku.com',
+  :enable_starttls_auto  =>  true
+}
