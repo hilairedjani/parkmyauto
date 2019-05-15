fecd7c3d1c569470050b3837282ee80b5ebc25c4
hilairedjani <djanilewis1@gmail.com>
2019-05-15 14:29:42 -0400

    Configure mysql DB

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
     def home
         if current_user
-            redirect_to user_vehicles_path(current_user)
+            if current_user.role.admin?
+                redirect_to all_vehicles_path
+            else
+                redirect_to user_vehicles_path(current_user)
+            end
         end
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_dispatch.tld_length = 2
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'hilairedjani',
+    :password => 'Hilaire6*',
+    :domain => 'parkmyauto.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
